1dd7f783603ee208ec6e6b032b28e0dccb41109a
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-09 15:28:42 +0900

    add: main.dartのUIを変更
    refs #5 問題文、選択肢2つを表示させるため

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
@@ -30,14 +32,80 @@ class _QuizPageState extends State<QuizPage> {
         image: DecorationImage(
             image: AssetImage('assets/background.jpg'), fit: BoxFit.cover),
       ),
-      child: const SafeArea(
+      child: SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Text(
-            '仮です',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
+            backgroundColor: Colors.transparent,
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    const Expanded(
+                      flex: 16,
+                      child: Center(
+                        child: Text(
+                          '問題文',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(0),
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            '選択肢1',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Spacer(
+                      flex: 1,
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(0),
+                                  ),
+                                )),
+                            child: const Text(
+                              '選択肢2',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            )),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )),
       ),
     );
   }
