7f36e80374c1d511c87eef6e66ca9bfb8621c7ae
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-09 17:29:38 +0900

    add: プロパティ内に問題文、選択肢を追加
    refs #9 問題を増やすため

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -7,6 +7,13 @@ class QuizBrain {
     Quiz(quizTitle: '出身地は？', quizAnswer1: '岐阜', quizAnswer2: '富山'),
     Quiz(quizTitle: '出身高校は？', quizAnswer1: '大垣南高校', quizAnswer2: '大垣東高校'),
     Quiz(quizTitle: '中学時代の部活は？', quizAnswer1: '陸上部', quizAnswer2: '野球部'),
+    Quiz(quizTitle: '高校時代の部活は？', quizAnswer1: '陸上部', quizAnswer2: '野球部'),
+    Quiz(quizTitle: '好きな球団は？', quizAnswer1: '中日', quizAnswer2: '巨人'),
+    Quiz(quizTitle: '長く続けたバイトは？', quizAnswer1: '結婚式場', quizAnswer2: 'ポールスミス'),
+    Quiz(quizTitle: '趣味は？', quizAnswer1: '読書', quizAnswer2: '筋トレ'),
+    Quiz(quizTitle: '最近ハマっている小説ジャンルは？', quizAnswer1: 'ミステリ', quizAnswer2: '恋愛'),
+    Quiz(quizTitle: '使っているPCは？', quizAnswer1: 'MAC', quizAnswer2: 'Windows'),
+    Quiz(quizTitle: '最近ハマっているアパレルブランドは？', quizAnswer1: 'Traditional Weather Wear', quizAnswer2: 'Mackintosh'),
   ];
 
   String getTitle() {
