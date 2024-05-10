94ab52be313f1a1ae3296843ce58740ee0cde904
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-10 09:28:35 +0900

    add: answerBrainリストを追加
    refs #10 答え合わせするために、答えを格納

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:sample_quiz/quiz.dart';
 
 class QuizBrain {
@@ -12,11 +14,23 @@ class QuizBrain {
     Quiz(quizTitle: '長く続けたバイトは？', quizAnswer1: '結婚式場', quizAnswer2: 'ポールスミス'),
     Quiz(quizTitle: '趣味は？', quizAnswer1: '読書', quizAnswer2: '筋トレ'),
     Quiz(quizTitle: '最近ハマっている小説ジャンルは？', quizAnswer1: 'ミステリ', quizAnswer2: '恋愛'),
-    Quiz(quizTitle: '使っているPCは？', quizAnswer1: 'MAC', quizAnswer2: 'Windows'),
+    Quiz(quizTitle: '使っているPCは？', quizAnswer1: 'Mac', quizAnswer2: 'Windows'),
     Quiz(
         quizTitle: '最近ハマっているアパレルブランドは？',
         quizAnswer1: 'Traditional Weather Wear',
         quizAnswer2: 'Mackintosh'),
+  ];
+
+  List<String> answerBrain = [
+    '岐阜',
+    '大垣南高校',
+    '野球部',
+    '陸上部',
+    '中日',
+    'ポールスミス',
+    '読書',
+    '恋愛',
+    'Mac'
   ];
 
   String getTitle() {
